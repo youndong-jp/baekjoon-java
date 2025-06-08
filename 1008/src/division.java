9737cf87c62d42b08613f71bb39e0eeae25cd83e
yooundong-jp <imyour8572@gmail.com>
2025-06-08 17:56:39 +0900

    Day2 백준 입출력과 사칙연산 2번 부터 6번까 성공~~git add .

---
+++ 1008/src/division.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class division {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+        System.out.println(a/b);
+}
+}
