7aa48f9238ac23e235860df57432ed989b2b432c
yooundong-jp <imyour8572@gmail.com>
2025-06-11 14:07:26 +0900

    백준 18108번 불기->서기 문제 풀이

---
+++ 18108/src/Year.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Year {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        sc.nextLine();
+        System.out.println(year-543);
+    }
+}
