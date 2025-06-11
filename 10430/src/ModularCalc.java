ca88c431810fb0d1e72b7987610fac914734930d
yooundong-jp <imyour8572@gmail.com>
2025-06-11 14:22:13 +0900

    백준 10430 문제 추가

---
+++ 10430/src/ModularCalc.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ModularCalc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        sc.nextLine();
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println(((a*b)%c));
+        System.out.println(((a%c)*(b%c)%c));
+    }
+}
