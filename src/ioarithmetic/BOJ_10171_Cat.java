0e634be5caa901c20133852e586b53544076a470
yooundong-jp <imyour8572@gmail.com>
2025-06-16 12:34:01 +0900

    백준 고양이 문제 풀이

--- src/ioarithmetic/BOJ_10171_Cat.java
+++ src/ioarithmetic/BOJ_10171_Cat.java
@@ -1,4 +1,13 @@
 package ioarithmetic;
+import java.util.Scanner;
 
 public class BOJ_10171_Cat {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+
 }
