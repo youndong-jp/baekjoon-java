cea3a3faae6df436ae7e22aa977f8681c09c7239
yooundong-jp <imyour8572@gmail.com>
2025-06-19 17:32:48 +0900

    feat/2884문제 해결/gpt한테 물어봄

--- src/condition/BOJ_2884_AlarmClock.java
+++ src/condition/BOJ_2884_AlarmClock.java
@@ -1,33 +1,19 @@
 package condition;
 import java.util.Scanner;
 public class BOJ_2884_AlarmClock {
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         Scanner sc = new Scanner(System.in);
         int H = sc.nextInt(); //시간 시
         int M = sc.nextInt(); //시간 분
-        int Minute = (M-45); //45분 줄이기
-        int LessMinute = ((60+M)-45); // 분이 부족할때 계산
-        int Hour = (H - 1);//시간 계산
-        if(H == 0) {
-            H = 24;
-            int ZeroHour = (H - 1);//시간 계산
-            if (M > 45) {
-                System.out.print(ZeroHour + " " + Minute);
-            } else if (M < 45) {
-                System.out.print(ZeroHour + " " + LessMinute);
-            }else  {
-                System.out.print("0" + " " + "0");
+        M -= 45;
+        if (M<0) {
+            M += 60;
+            H -= 1;
+            if (H < 0) {
+                H = 23;
             }
-        }else{
-            if (M > 45) {
-                System.out.print(H + " " + Minute);
-            } else if (M < 45) {
-                System.out.print(Hour + " " + LessMinute);
-            }else{
-                System.out.print(H + " " + Minute);
-            }
-
         }
+        System.out.println(H+" "+M);
     }
-
 }
+
