397c53995adf1b36ff19424b50523bdc525ab96a
yooundong-jp <imyour8572@gmail.com>
2025-06-21 16:04:28 +0900

    feat : 10950 문제 추가

---
+++ src/loop/BOJ_10950_Ab3.java
@@ -0,0 +1,14 @@
+package loop;
+import java.util.Scanner;
+
+public class BOJ_10950_Ab3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(n-->0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
