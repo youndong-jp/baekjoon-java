f974b6f67e73ba31ff8c25ac2f30427f32c0bbda
yooundong-jp <imyour8572@gmail.com>
2025-06-22 20:26:12 +0900

    feat : 8393 합 문제 추가

---
+++ src/loop/BOJ_8393_Sum.java
@@ -0,0 +1,14 @@
+package loop;
+import java.util.Scanner;
+
+public class BOJ_8393_Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
