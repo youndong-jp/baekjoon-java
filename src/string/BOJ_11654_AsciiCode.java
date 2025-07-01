3a3c83867a42204b276c6c902456090b61b07ad8
yooundong-jp <imyour8572@gmail.com>
2025-07-01 11:24:58 +0900

    feat : 11654 아스키코드 문제 추가

---
+++ src/string/BOJ_11654_AsciiCode.java
@@ -0,0 +1,10 @@
+package string;
+import java.util.Scanner;
+
+public class BOJ_11654_AsciiCode {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        System.out.println((int)ch);
+    }
+}
