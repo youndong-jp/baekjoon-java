58657e26bf52e31e705a27df6148e3060ec8ae23
yooundong-jp <imyour8572@gmail.com>
2025-07-21 15:01:58 +0900

    feat : 킹, 퀸, 룩, 비숍, 나이트, 폰 문제추가

---
+++ src/string/BOJ_3003_Chess.java
@@ -0,0 +1,13 @@
+package string;
+import java.util.Scanner;
+
+public class BOJ_3003_Chess {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] chess = {1, 1, 2, 2, 2, 8};
+        for (int i = 0; i < 6; i++) {
+            int piece = sc.nextInt();
+            System.out.print(chess[i]-piece+" ");
+        }
+    }
+}
